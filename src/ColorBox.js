1de8c4f0ed04ee13d912a4ff873b659625accf4d
Doug Jones <DSJR1284@GMAIL.COM>
2021-10-26 06:26:52 -0400

    commit tree 64a4f45dfb78564c1faf1b0e7f75aad867445cf3

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -1,3 +1,4 @@
+import { ReactComponent } from '*.svg';
 import React, { Component } from 'react';
 
 export default class ColorBox extends Component {
@@ -11,10 +12,8 @@ export default class ColorBox extends Component {
   render() {
     return (
       <div className="color-box" style={{opacity: this.props.opacity}}>
-        {this.props.opacity >= 0.2 ? < ColorBox opacity={this.props.opacity  -0.1} /> : null}
+        {this.props.opacity >= 0.2 ? < ColorBox opacity={this.props.opacity -0.1}/> : null}
       </div>
     )
   }
-
 }
-
